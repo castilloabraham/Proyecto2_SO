b5620d2f5f2fa1528144cbabd6ffd28bbb428c33
Luis Rivera <luis.rivera@correo.unimet.edu.ve>
2026-03-24 01:09:32 -0400

    feat: agrega propietario a directorios

--- SimuladorArchivos/src/main/java/modelo/Directorio.java
+++ SimuladorArchivos/src/main/java/modelo/Directorio.java
@@ -4,39 +4,34 @@ import estructuras.ListaEnlazada;
 
 public class Directorio {
     private String nombre;
+    private String propietario;
     private ListaEnlazada<Archivo> archivos;
     private ListaEnlazada<Directorio> subdirectorios;
 
     public Directorio(String nombre) {
+        this(nombre, "admin");
+    }
+
+    public Directorio(String nombre, String propietario) {
         this.nombre = nombre;
-        // Inicializamos tus propias estructuras
+        this.propietario = propietario;
         this.archivos = new ListaEnlazada<>();
         this.subdirectorios = new ListaEnlazada<>();
     }
 
-    // Getters y Setters
-    public String getNombre() {
-        return nombre;
-    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public String getPropietario() { return propietario; }
+    public void setPropietario(String propietario) { this.propietario = propietario; }
 
-    public ListaEnlazada<Archivo> getArchivos() {
-        return archivos;
-    }
+    public ListaEnlazada<Archivo> getArchivos() { return archivos; }
+    public ListaEnlazada<Directorio> getSubdirectorios() { return subdirectorios; }
 
-    public ListaEnlazada<Directorio> getSubdirectorios() {
-        return subdirectorios;
-    }
-    
-    // Método rápido para agregar un archivo a esta carpeta
     public void agregarArchivo(Archivo archivo) {
         this.archivos.agregar(archivo);
     }
-    
-    // Método rápido para agregar una subcarpeta
+
     public void agregarSubdirectorio(Directorio dir) {
         this.subdirectorios.agregar(dir);
     }
